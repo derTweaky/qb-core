2fe26400ac8fcb0d298b44fdefbb47c3bf5d8c42
derTweaky <106425590+derTweaky@users.noreply.github.com>
2022-08-30 17:44:14 +0200

    update de.lua

--- locale/de.lua
+++ locale/de.lua
@@ -2,27 +2,120 @@ local Translations = {
     error = {
         not_online = 'Spieler nicht online',
         wrong_format = 'Falsches Format',
-        missing_args = 'Nicht jedes Argument wurde angegeben (x, y, z)',
+        missing_args = 'Nicht jedes Argument wurde eingegeben (x, y, z)',
         missing_args2 = 'Alle Argumente müssen ausgefüllt werden!',
         no_access = 'Kein Zugriff auf diesen Befehl',
-        company_too_poor = 'Dein Arbeitgeber ist pleite',
+        company_too_poor = 'Ihr Arbeitgeber ist pleite',
         item_not_exist = 'Item existiert nicht',
         too_heavy = 'Inventar zu voll',
+        location_not_exist = 'Der Standort existiert nicht',
         duplicate_license = 'Doppelte Rockstar-Lizenz gefunden',
         no_valid_license  = 'Keine gültige Rockstar-Lizenz gefunden',
-        not_whitelisted = 'Du bist nicht gewhitelistet für diesen Server'
+        not_whitelisted = 'Du bist nicht auf der Whitelist für diesen Server',
+        server_already_open = 'Der Server ist bereits geöffnet',
+        server_already_closed = 'Der Server ist bereits geschlossen',
+        no_permission = 'Du hast keine Berechtigung dafür...',
+        no_waypoint = 'Kein Wegpunkt gesetzt.',
+        tp_error = 'Fehler beim Teleportieren.',
     },
-    success = {},
+    success = {
+        server_opened = 'Der Server wurde geöffnet',
+        server_closed = 'Der Server wurde geschlossen',
+        teleported_waypoint = 'Zum Wegpunkt teleportiert.',
+    },
     info = {
-        received_paycheck = 'Du hast deinen Gehalt über $%{value} erhalten',
-        job_info = 'Beruf: %{value} | Dienstgrad: %{value2} | im Dienst: %{value3}',
+        received_paycheck = 'Du hast dein ein Gehaltsscheck von $%{value} erhalten.',
+        job_info = 'Job: %{value} | Rang: %{value2} | Dienstbereitschft: %{value3}',
         gang_info = 'Gang: %{value} | Rang: %{value2}',
         on_duty = 'Du bist jetzt im Dienst!',
         off_duty = 'Du bist jetzt außer Dienst!',
-        checking_ban = 'Hallo %s. Wir prüfen gerade, ob du gebannt bist.',
-        join_server = 'Willkommen %s bei {Server Name}.',
-        checking_whitelisted = 'Hallo %s. Wir überprüfen gerade deine Erlaubnis.'
-    }
+        checking_ban = 'Hallo %s. Wir prüfen, ob Sie gebannt bist.',
+        join_server = 'Willkommen bei %s {Server Name}.',
+        checking_whitelisted = 'Hallo %s. Wir prüfen Ihren Zugangsberechtigung.',
+        exploit_banned = 'Du wurdest wegen Cheating gebannt. Besuche unseren Discord für weitere Informationen: %{discord}',
+        exploit_dropped = 'Du wurdest wegen Exploitnutzung gekickt:',
+    },
+    command = {
+        tp = {
+            help = 'TP To Player or Coords (Admin Only)',
+            params = {
+                x = { name = 'id/x', help = 'ID of player or X position'},
+                y = { name = 'y', help = 'Y position'},
+                z = { name = 'z', help = 'Z position'},
+            },
+        },
+        tpm = { help = 'TP To Marker (Admin Only)' },
+        togglepvp = { help = 'Toggle PVP on the server (Admin Only)' },
+        addpermission = {
+            help = 'Give Player Permissions (God Only)',
+            params = {
+                id = { name = 'id', help = 'ID of player' },
+                permission = { name = 'permission', help = 'Permission level' },
+            },
+        },
+        removepermission = {
+            help = 'Remove Player Permissions (God Only)',
+            params = {
+                id = { name = 'id', help = 'ID of player' },
+                permission = { name = 'permission', help = 'Permission level' },
+            },
+        },
+        openserver = { help = 'Open the server for everyone (Admin Only)' },
+        closeserver = {
+            help = 'Close the server for people without permissions (Admin Only)',
+            params = {
+                reason = { name = 'reason', help = 'Reason for closing (optional)' },
+            },
+        },
+        car = {
+            help = 'Spawn Vehicle (Admin Only)',
+            params = {
+                model = { name = 'model', help = 'Model name of the vehicle' },
+            },
+        },
+        dv = { help = 'Delete Vehicle (Admin Only)' },
+        givemoney = {
+            help = 'Give A Player Money (Admin Only)',
+            params = {
+                id = { name = 'id', help = 'Player ID' },
+                moneytype = { name = 'moneytype', help = 'Type of money (cash, bank, crypto)' },
+                amount = { name = 'amount', help = 'Amount of money' },
+            },
+        },
+        setmoney = {
+            help = 'Set Players Money Amount (Admin Only)',
+            params = {
+                id = { name = 'id', help = 'Player ID' },
+                moneytype = { name = 'moneytype', help = 'Type of money (cash, bank, crypto)' },
+                amount = { name = 'amount', help = 'Amount of money' },
+            },
+        },
+        job = { help = 'Check Your Job' },
+        setjob = {
+            help = 'Set A Players Job (Admin Only)',
+            params = {
+                id = { name = 'id', help = 'Player ID' },
+                job = { name = 'job', help = 'Job name' },
+                grade = { name = 'grade', help = 'Job grade' },
+            },
+        },
+        gang = { help = 'Check Your Gang' },
+        setgang = {
+            help = 'Set A Players Gang (Admin Only)',
+            params = {
+                id = { name = 'id', help = 'Player ID' },
+                gang = { name = 'gang', help = 'Gang name' },
+                grade = { name = 'grade', help = 'Gang grade' },
+            },
+        },
+        ooc = { help = 'OOC Chat Message' },
+        me = {
+            help = 'Show local message',
+            params = {
+                message = { name = 'message', help = 'Message to send' }
+            },
+        },
+    },
 }
 
 Lang = Locale:new({
